02df33041f1526f97629f4e80389ad5a0082ec2c
Tint Naing Win <tintnaingwin.code@gmail.com>
2026-03-24 11:07:09 +0700

    chore: fix constant declaration syntax in SmspohApi

--- src/SmspohApi.php
+++ src/SmspohApi.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 
 class SmspohApi
 {
-    public const string ENDPOINT = 'https://v3.smspoh.com/api/rest/send';
+    public const ENDPOINT = 'https://v3.smspoh.com/api/rest/send';
 
     protected HttpClient $client;
 
